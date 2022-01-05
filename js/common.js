4f2dfc8865fabbbeb1d874cfaac5f6fc01ad3d1d
powerwmj <95343811+powerwmj@users.noreply.github.com>
2022-01-05 22:56:53 +0900

    up

---
+++ js/common.js
@@ -0,0 +1,10 @@
+
+$('header .hd button').on('click',function(){
+    alert('hoit')
+    $('.s_mn').toggle();
+})
+
+$('.family a').on('click', function(){
+    alert('hoit')
+    $('.all_fam').toggle();
+})
---
+++ js/inc.js
@@ -0,0 +1,6 @@
+
+$('.wrap').prepend('<header></header>')
+.append('<footer></footer>');
+
+$('header').load('inc.html header .header_w');
+$('footer').load('inc.html footer .footer_w');
