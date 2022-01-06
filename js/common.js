964e1d57562cdacb575df1609c1169db27025ef1
powerwmj <95343811+powerwmj@users.noreply.github.com>
2022-01-06 13:10:47 +0900

    sub

--- js/common.js
+++ js/common.js
@@ -1,10 +1,3 @@
 
-$('header .hd button').on('click',function(){
-    alert('hoit')
-    $('.s_mn').toggle();
-})
 
-$('.family a').on('click', function(){
-    alert('hoit')
-    $('.all_fam').toggle();
-})
+
--- js/inc.js
+++ js/inc.js
@@ -2,5 +2,17 @@
 $('.wrap').prepend('<header></header>')
 .append('<footer></footer>');
 
-$('header').load('inc.html header .header_w');
-$('footer').load('inc.html footer .footer_w');
+$('header').load('inc.html header .header_w',menu);
+$('footer').load('inc.html footer .footer_w',family);
+
+function menu(){
+    $('header .hd button').on('click',function(){
+        $('.s_mn').toggle();
+    })
+}
+
+function family(){
+    $('.family a').on('click', function(){
+        $('.all_fam').toggle();
+    })
+}
