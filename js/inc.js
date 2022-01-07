dcbecd28fd2f3f1327c2fbd1a67172a6581e820b
powerwmj <95343811+powerwmj@users.noreply.github.com>
2022-01-07 09:52:56 +0900

    index link

--- js/inc.js
+++ js/inc.js
@@ -7,6 +7,7 @@ $('footer').load('inc.html footer .footer_w',family);
 
 function menu(){
     $('header .hd button').on('click',function(){
+        $('header .hd button').toggleClass('close');
         $('.s_mn').toggleClass('active');
     })
 }
