c9f0fafdac51ccb0f7fb474f0db3d3812ae597ec
powerwmj <95343811+powerwmj@users.noreply.github.com>
2022-01-10 11:38:13 +0900

    slick up

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 
+$('.slider_w ul').slick({
+    // infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrow: true,
+    prevArrow: '.slider_w .btns a.prev',
+    nextArrow: '.slider_w .btns a.next',
+     
+});
 
-
+$('.slider_w ul').slick('goTo', 1);  
